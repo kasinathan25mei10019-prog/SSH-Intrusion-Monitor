2a03babaebeb269878e16433d9f982609ab6a2e3
kasinathan25mei10019-prog <kasinathan.25mei10019@vitbhopal.ac.in>
2025-11-20 17:06:25 +0530

    Update SSH_Monitor.py
    The patch did reworks on line_split module so that it can analyse
    or monitor real-world Linux logs

--- SSH_Monitor.py
+++ SSH_Monitor.py
@@ -1,20 +1,23 @@
 import time
-def line_split(line):
-    part1 = line.split('] ',1)
-    time = part1[0]
-    other = part1[1]
-    part2 = other.split(' ',3)
-    ip = part2[0]
-    user = part2[1]
-    event = part2[2]
-    message = part2[3]
-    return {
-        'time':time,
-        'ip': ip,
-        'user':user,
-        'envent':event,
-        'message':message
-    }
+import re
+log_line = re.compile(
+    r'(?P<timestamp>\w{3}\s+\d{1,2}\s+\d{2}:\d{2}:\d{2}).*?'
+    r'(?:Failed password | Accepted password).*?'
+    r'for\s+(?P<user>\S+).*?'
+    r'from\s+(?P<ip>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'
+)
+def line_re(line):
+    match = log_line.search(line)
+    if match:
+        entry = match.groupdict()
+        if 'Accepted password' in line:
+            entry['event'] = 'SUCCESS'
+            entry['message'] = 'User login accepted'
+        else:
+            entry['event'] = 'FAILURE'
+            entry['message'] = 'User login failed'
+        return entry
+    return None
 
 def monitoring(log_file):
     file = open(log_file)
@@ -24,7 +27,7 @@ def monitoring(log_file):
         if not line:
             time.sleep(1)
             continue
-        log_details = line_split(line.strip())
+        log_details = line_re(line)
         if log_details:
             activity(log_details)
 
@@ -36,7 +39,7 @@ def activity(log_details):
     if event_info == "SUCCESS":
         print(f"[{current_time}] [SUCCESS] User : {user_info} has been logged in successfully from IP : {ip_info}")
     if event_info == "FAILURE":
-        print(f"[{current_time}] [FALURE] User : {user_info} Failed log in attempt from IP : {ip_info}")
+        print(f"[{current_time}] [FAILURE] User : {user_info} Failed log in attempt from IP : {ip_info}")
 
 path_log = input("Enter the full path to the log file ")
 monitoring(path_log)
