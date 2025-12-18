6b273a33f742af6f71919aa2cf5ac263465505eb
s0suk3 <njkasinathan2007@gmail.com>
2025-12-18 22:29:35 +0530

    added functions to block ip address with addition of this the script need root privilage

--- SSH_Monitor.py
+++ SSH_Monitor.py
@@ -1,6 +1,8 @@
 import time
 import re
 from collections import defaultdict, deque
+import os
+
 log_line = re.compile(
     r'(?P<timestamp>\w{3}\s+\d{1,2}\s+\d{2}:\d{2}:\d{2}).*?'
     r'(?:Failed password | Accepted password).*?'
@@ -8,7 +10,8 @@ log_line = re.compile(
     r'from\s+(?P<ip>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'
 )
 FAILED_LIMIT = 5
-FAILED_WINDOW = 300  # seconds
+FAILED_WINDOW = 300
+BLOCKED_IP_FILE = "blocked_ips.txt"
 
 failed_attempts = {}
 
@@ -39,6 +42,23 @@ def detect_bruteforce(ip):
         return True
     return False
 
+def is_ip_blocked(ip):
+    if not os.path.exists(BLOCKED_IP_FILE):
+        return False
+
+    with open(BLOCKED_IP_FILE, "r") as file:
+        blocked_ips = file.read().splitlines()
+
+    return ip in blocked_ips
+
+def save_blocked_ip(ip):
+    with open(BLOCKED_IP_FILE, "a") as file:
+        file.write(ip + "\n")
+
+def block_ip(ip):
+    command = f"iptables -A INPUT -s {ip} -j DROP"
+    os.system(command)
+
 def monitoring(log_file):
     file = open(log_file)
     print("Starting the analyser.......")
@@ -64,11 +84,14 @@ def activity(log_details):
 
     else:
         attack = detect_bruteforce(ip_info)
-
         print(f"[{current_time}] [FAILURE] User {user_info} failed login from {ip_info}")
-
         if attack:
-            print(f"[{current_time}] [ALERT] Brute-force attack detected from {ip_info}")
+            if not is_ip_blocked(ip_info):
+                save_blocked_ip(ip_info)
+                block_ip(ip_info)
+                print(f"[{current_time}] [ALERT] Brute-force attack detected from {ip_info}")
+            else:
+                print(f"[{current_time}] [INFO] IP {ip_info} is already blocked.")
 
 path_log = input("Enter the full path to the log file ")
 monitoring(path_log)
