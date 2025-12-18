36226a742f134dae7162769a5ef538518d21435b
s0suk3 <njkasinathan2007@gmail.com>
2025-12-18 22:20:51 +0530

    Detection of Bruteforce attacks

--- SSH_Monitor.py
+++ SSH_Monitor.py
@@ -1,11 +1,17 @@
 import time
 import re
+from collections import defaultdict, deque
 log_line = re.compile(
     r'(?P<timestamp>\w{3}\s+\d{1,2}\s+\d{2}:\d{2}:\d{2}).*?'
     r'(?:Failed password | Accepted password).*?'
     r'for\s+(?P<user>\S+).*?'
     r'from\s+(?P<ip>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'
 )
+FAILED_LIMIT = 5
+FAILED_WINDOW = 300  # seconds
+
+failed_attempts = {}
+
 def line_re(line):
     match = log_line.search(line)
     if match:
@@ -18,6 +24,20 @@ def line_re(line):
             entry['message'] = 'User login failed'
         return entry
     return None
+
+def detect_bruteforce(ip):
+    now = time.time()
+    if ip not in failed_attempts:
+        failed_attempts[ip] = []
+    failed_attempts[ip].append(now)
+    valid_attempts = []
+    for t in failed_attempts[ip]:
+        if now - t <= FAILED_WINDOW:
+            valid_attempts.append(t)
+    failed_attempts[ip] = valid_attempts
+    if len(valid_attempts) >= FAILED_LIMIT:
+        return True
+    return False
 
 def monitoring(log_file):
     file = open(log_file)
@@ -37,9 +57,18 @@ def activity(log_details):
     user_info = log_details['user']
     current_time = time.strftime('%H:%M:%S')
     if event_info == "SUCCESS":
-        print(f"[{current_time}] [SUCCESS] User : {user_info} has been logged in successfully from IP : {ip_info}")
-    if event_info == "FAILURE":
-        print(f"[{current_time}] [FAILURE] User : {user_info} Failed log in attempt from IP : {ip_info}")
+        if ip_info in failed_attempts:
+            del failed_attempts[ip_info]
+
+        print(f"[{current_time}] [SUCCESS] User {user_info} logged in from {ip_info}")
+
+    else:
+        attack = detect_bruteforce(ip_info)
+
+        print(f"[{current_time}] [FAILURE] User {user_info} failed login from {ip_info}")
+
+        if attack:
+            print(f"[{current_time}] [ALERT] Brute-force attack detected from {ip_info}")
 
 path_log = input("Enter the full path to the log file ")
 monitoring(path_log)
